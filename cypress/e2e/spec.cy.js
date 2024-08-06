37d253066287a4f22980ee42825ac877d9d05907
Sérgio Adriano <sergioadrianomc@gmail.com>
2024-08-06 15:46:06 -0300

    correção do test no button 'deletar'

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -11,7 +11,7 @@ describe('Agenda Contatos App', () => {
     cy.get('input[placeholder="Nome"]').type('John Doe');
     cy.get('input[placeholder="E-mail"]').type('john.doe@example.com');
     cy.get('input[placeholder="Telefone"]').type('123456789');
-    cy.get('form').contains('Adicionar').click();
+    cy.contains('Adicionar').click();
 
     cy.contains('div.contato', 'John Doe').should('exist');
     cy.contains('div.contato', 'john.doe@example.com').should('exist');
@@ -23,7 +23,7 @@ describe('Agenda Contatos App', () => {
     cy.get('input[placeholder="Nome"]').type('Jane Doe');
     cy.get('input[placeholder="E-mail"]').type('jane.doe@example.com');
     cy.get('input[placeholder="Telefone"]').type('987654321');
-    cy.get('form').contains('Adicionar').click();
+    cy.contains('Adicionar').click();
 
     // Ensure the contact was added
     cy.contains('div.contato', 'Jane Doe').should('exist');
@@ -33,7 +33,7 @@ describe('Agenda Contatos App', () => {
       cy.contains('button', 'Editar').click();
     });
     cy.get('input[placeholder="Nome"]').clear().type('Jane Smith');
-    cy.get('form').contains('Salvar').click();
+    cy.contains('Salvar').click();
 
     // Ensure the contact was edited
     cy.contains('div.contato', 'Jane Smith').should('exist');
@@ -45,17 +45,20 @@ describe('Agenda Contatos App', () => {
     cy.get('input[placeholder="Nome"]').type('Bob Smith');
     cy.get('input[placeholder="E-mail"]').type('bob.smith@example.com');
     cy.get('input[placeholder="Telefone"]').type('123123123');
-    cy.get('form').contains('Adicionar').click();
+    cy.contains('Adicionar').click();
 
     // Ensure the contact was added
     cy.contains('div.contato', 'Bob Smith').should('exist');
 
     // Delete the contact
     cy.contains('div.contato', 'Bob Smith').within(() => {
-      cy.contains('button', 'Remover').click();
+      cy.contains('button', 'Deletar').click();
     });
 
+    // Wait for some time to ensure the removal process is completed
+    cy.wait(2000);
+
     // Ensure the contact was deleted
-    cy.contains('div.contato', 'Bob Smith').should('not.exist');
+    cy.contains('div.contato', 'Bob Smith', { timeout: 10000 }).should('not.exist');
   });
 });
